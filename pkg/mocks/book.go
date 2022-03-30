25086241589ee8fdaca89c83ca0a2ab495063c42
sohan patel <sohan.m.patel@leidos.com>
2022-03-29 22:58:26 -0400

    progress push

---
+++ pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "github.com/sohanpatel1221/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "Golang",
+		Author: "Gopher",
+		Desc:   "A book for Go",
+	},
+}
